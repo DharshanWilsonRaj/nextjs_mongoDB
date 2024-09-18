c6df15b491c1180b66b680633bce37725de7710f
Dharshan <dharshan@selvisoftware.com>
2024-09-18 16:28:57 +0530

    users

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -2,15 +2,20 @@
 
 import React from 'react'
 export default async function page() {
+    try {
+        const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
+        const data = await response.json() || [];
+        return (
+            <div>
+                users
+                {data?.length && data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{ textTransform: "uppercase" }}>{curr?.name}</span> </h1>)}
+            </div>
+        )
+    } catch (error) {
+        console.error(error);
+        return <div>Error occurred while fetching data</div>
+    }
 
-    const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
-    const data = await response.json() || [];
-    return (
-        <div>
-            users
-            {data?.length && data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{ textTransform: "uppercase" }}>{curr?.name}</span> </h1>)}
-        </div>
-    )
 }
 
 
