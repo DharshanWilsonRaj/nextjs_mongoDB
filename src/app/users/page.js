5f267d5627d9b0134ea4bee01ea355b28c646d9d
Dharshan <dharshan@selvisoftware.com>
2024-09-18 16:19:28 +0530

    user page fixed

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,46 +1,16 @@
 
 
 import React from 'react'
-
-export const dynamic = 'force-dynamic'
 export default async function page() {
-    const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
-    const data = await response.json() || [];
-    console.log(data);
+    // const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
+    // const data = await response.json() || [];
     return (
         <div>
-            {data?.length && data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{ textTransform: "uppercase" }}>{curr?.name}</span> </h1>)}
+            users
+            {/* {data?.length && data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{ textTransform: "uppercase" }}>{curr?.name}</span> </h1>)} */}
         </div>
     )
 }
 
 
-
-// import React from 'react'
-// import { useEffect } from 'react'
-// import { useState } from 'react'
-
-// const UserPage = () => {
-//     const [data, setData] = useState([])
-//     useEffect(() => {
-//         async function getData() {
-//             const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
-//             const responseData = await response.json() || [];
-//             // setData(responseData || [])
-//             console.log(responseData);
-//         }
-//         getData()
-//     }, [])
-
-//     return (
-//         <div>
-//             <div>
-//                 Users
-//                 {data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{ textTransform: "uppercase" }}>{curr?.name}</span> </h1>)}
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default UserPage
 
