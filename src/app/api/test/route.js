5a8fb86d15802f1c2b4e6eb5c17db78b0eab4e02
Dharshan <dharshan@selvisoftware.com>
2024-09-18 14:59:50 +0530

    mongo db commted

---
+++ src/app/api/test/route.js
@@ -0,0 +1,23 @@
+import clientPromise from "@/app/lib/mongo";
+
+export async function GET(req) {
+  try {
+    const client = await clientPromise;  // Get MongoDB client
+    const db = client.db('TESTDB');  // Connect to the database
+
+    // Query the neighborhoods collection and get all restaurants
+    const neighborhoods = await db.collection('TESTMONGODB').find({}).toArray();
+
+    // Return the list of neighborhoods as a JSON response
+    return new Response(JSON.stringify(neighborhoods), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ message: 'MongoDB connection or query failed', error }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+}
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const page = () => {
+export default async function page() {
+    const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
+    const data = await response.json() || [];
     return (
         <div>
-            page
+            {data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{textTransform:"uppercase"}}>{curr?.name}</span> </h1>)}
         </div>
     )
 }
 
-export default page
+
