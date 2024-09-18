b4cc5375c551bbd7cd377b4aa75b5223c5dd7d1e
Dharshan <dharshan@selvisoftware.com>
2024-09-18 15:13:32 +0530

    mango db commit

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,13 +1,37 @@
-import React from 'react'
+// import React from 'react'
 
-export default async function page() {
-    const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
-    const data = await response.json() || [];
+// export default async function page() {
+//     const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
+//     const data = await response.json() || [];
+//     return (
+//         <div>
+//             {data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{textTransform:"uppercase"}}>{curr?.name}</span> </h1>)}
+//         </div>
+//     )
+// }
+
+"use client"
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react/cjs/react.production.min'
+
+const UserPage = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        async function getData() {
+            const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
+            const responseData = await response.json() || [];
+            setData(responseData || [])
+        }
+        getData()
+    }, [])
     return (
         <div>
-            {data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{textTransform:"uppercase"}}>{curr?.name}</span> </h1>)}
+            <div>
+                {data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{ textTransform: "uppercase" }}>{curr?.name}</span> </h1>)}
+            </div>
         </div>
     )
 }
 
-
+export default UserPage
