0d9e3aceffb0d9195d54dd7d36d21f5480d9ae2c
Dharshan <dharshan@selvisoftware.com>
2024-09-18 15:23:04 +0530

    test

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,19 +1,20 @@
-// import React from 'react'
 
-// export default async function page() {
-//     const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
-//     const data = await response.json() || [];
-//     return (
-//         <div>
-//             {data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{textTransform:"uppercase"}}>{curr?.name}</span> </h1>)}
-//         </div>
-//     )
-// }
 
-// "use client"
+import React from 'react'
+export default async function page() {
+    const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
+    const data = await response.json() || [];
+   
+    return (
+        <div>
+            {data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{ textTransform: "uppercase" }}>{curr?.name}</span> </h1>)}
+        </div>
+    )
+}
+
 // import React from 'react'
 // import { useEffect } from 'react'
-// import { useState } from 'react/cjs/react.production.min'
+// import { useState } from 'react'
 
 // const UserPage = () => {
 //     const [data, setData] = useState([])
@@ -21,13 +22,16 @@
 //         async function getData() {
 //             const response = await fetch(`${process.env.APP_BASE_URL}/api/test`);
 //             const responseData = await response.json() || [];
-//             setData(responseData || [])
+//             // setData(responseData || [])
+//             console.log(responseData);
 //         }
 //         getData()
 //     }, [])
+
 //     return (
 //         <div>
 //             <div>
+//                 Users
 //                 {data?.map((curr, idx) => <h1 key={idx}> UserName: <span style={{ textTransform: "uppercase" }}>{curr?.name}</span> </h1>)}
 //             </div>
 //         </div>
@@ -35,13 +39,4 @@
 // }
 
 // export default UserPage
-import React from 'react'
-const page = () => {
-    return (
-        <div>
-            page
-        </div>
-    )
-}
 
-export default page
