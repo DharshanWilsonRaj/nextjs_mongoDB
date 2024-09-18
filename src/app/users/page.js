03942026002ecdd88ac093ca024394a42d0da0aa
Dharshan <dharshan@selvisoftware.com>
2024-09-18 15:17:10 +0530

    package installed

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -35,3 +35,13 @@
 // }
 
 // export default UserPage
+import React from 'react'
+const page = () => {
+    return (
+        <div>
+            page
+        </div>
+    )
+}
+
+export default page
